b6987be376757d7fda7c8e26a9dbc16a19d57678
LettyGoat <87225853+LettyGoat@users.noreply.github.com>
2022-04-13 15:10:46 +0100

    abc

---
+++ commands/fun/kiss.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+const discord = require("discord.js");
+
+module.exports = {
+  name: "kiss",
+  aliases: [""],
+  category: "fun",
+  description: "kiss a user",
+  usage: "<$kiss> [user]",
+  ownerOnly: false,
+  guildOnly: false,
+  requiredPerms: [],
+  enabled: true,
+  run: async (client, message, args) => {
+    if (!message.mentions.users.first()) {
+      return message.reply("You must mention the user that you want to kiss!");
+    }
+    if (message.mentions.users.first().id === message.author.id) {
+      return message.channel.send("some loner tryna kiss himself 💀");
+    }
+    try {
+      const response = await fetch(`https://nekos.life/api/kiss`);
+      const data = await response.json();
+      message.reply(data.url.toString());
+    } catch (err) {}
+  },
+};
